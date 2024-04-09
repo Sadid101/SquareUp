ae62b0b7ba9674dc63d76949212a2af977f2e293
Tausif Sadid <tausifsadid101@gmail.com>
2024-04-10 02:14:11 +0600

    feat: added wallet css

--- square-up/app/transactions/page.tsx
+++ square-up/app/transactions/page.tsx
@@ -4,7 +4,7 @@ export default function Transactions()  {
     return (
         <div className="h-svh font-sans-serif">
             <div className="fixed h-[30svh] w-svw">
-                <Image src={'/money.jpg'} layout="fill" objectFit="cover" alt="Cover Photo"/>
+                <Image src={'/money.jpg'} layout="fill" objectFit="cover" alt="Cover Photo" className="rounded-[20px]"/>
             </div>
             <div className="h-full pt-[30svh] bg-gradient-to-tl from-[#1d7a86] via-[#264d70] to-[#574e78]">
                 <div className="px-[15%]">
@@ -13,16 +13,19 @@ export default function Transactions()  {
                     </div>
                     <div className="text-white text-3xl font-semibold top-[-40px] relative">Office Meals</div>
                 </div>
-                <div className="mx-[15%] flex top-[-25px] relative justify-between items-center h-[25%] bg-[#123751] rounded-[20px] p-5">
-                    <div className="">
-                        <div>You are owed</div>
+                <div className="mx-[15%] flex top-[-25px] relative  rounded-[30px] p-3 bg-cover bg-center bg-[#C97629]">
+                    <div className="border-dashed border-[#A85825] border-y-[2px] w-full p-5 mx-5">
                         <div>
-                            <span>$</span>
-                            <span>2,500</span>
-                            <span>.00</span>
+                            <div>You are owed</div>
+                            <div>
+                                <span>$</span>
+                                <span>2,500</span>
+                                <span>.00</span>
+                            </div>
                         </div>
+                        <button className="px-5 mt-5 text-nowrap mr-2 p-2 rounded-lg text-white font-semibold bg-[#123751] max-w-fit">Square Up</button>
                     </div>
-                    <button className="px-5 text-nowrap bg-[#119611] mr-2 p-2 rounded-lg text-white font-semibold bg-[#123751]">Square Up</button>
+                    <Image src={'/WalletButton.svg'} layout="fill" alt="Cover Photo" objectFit="contain" className="object-cover object-center ml-auto !w-fit !h-[50%] mt-auto mb-auto "/>
                 </div>
                 <div className="flex overflow-x-scroll px-[15%] overflow-hidden" style={{scrollbarWidth: 'none'}}>
                     <button className="px-5 text-nowrap bg-[#119611] mr-2 p-2 rounded-lg text-white font-semibold bg-gradient-to-tl from-[#025602] via-[#037603] to-[#119611]">Square Up</button>
